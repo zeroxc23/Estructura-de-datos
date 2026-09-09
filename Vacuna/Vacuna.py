f08d9bc0d91db23ab016bbf047f9d8addd919cc9
Jair S2517 <88949020+JairS2517@users.noreply.github.com>
2026-09-09 17:50:49 -0500

    Update Vacuna.py

--- Vacuna/Vacuna.py
+++ Vacuna/Vacuna.py
@@ -1,34 +1,44 @@
 import time
 import os
 
-#  Contextualizacion de la vacuna
-print ("=============================================================",
-    "\n===                        HISTORIA                       ===",
-    "\n=============================================================",
-    "\nUn virus desconocido ha puesto en peligro a la humanidad,",
-    "\ntú, como científico, debes fabricar una vacuna utilizando 6",
-    "\ningredientes. Pero no es tan fácil... Debes elegir los ",
-    "\ningredientes correctos para que la vacuna funcione.",
-    "\n¡El destino del mundo está en tus manos!")
+#  Mostrar contextualizacion de la vacuna
+def MostrarIntroduccion():
+    print ("=============================================================",
+        "\n===                        HISTORIA                       ===",
+        "\n=============================================================",
+        "\n  Un virus desconocido ha puesto en peligro a la humanidad,",
+        "\n tú, como científico, debes fabricar una vacuna utilizando 6",
+        "\n   ingredientes. Pero no es tan fácil... Debes elegir los ",
+        "\n    ingredientes correctos para que la vacuna funcione.",
+        "\n        ¡El destino del mundo está en tus manos!")
+    input("\n--          Presiona enter para continuar          ---")
 
-input("\n--          Presiona enter para continuar          ---")
-time.sleep(1)
-os.system("cls")
-
-#  Menu principal
-print ("\n======================================================",
-    "\n===             FABRICACION DE VACUNAS            ===",
-    "\n======================================================",
-    "\n-- Ingredientes disponibles:",
-    "\n-- 1.",
-    "\n-- 2.",
-    "\n-- 3.",
-    "\n-- 4.",
-    "\n-- 5.",
-    "\n-- 6.",
-    "\n-- 7.",
-    "\n-- 8." \
-    "\n-- 9.",
-    "\n-- 10. Caldo de pollito.",
-    "\n-- Seleccione 6 ingredientes:")
-
+#  Mostrar reglas del juego
+def MostrarReglas():
+    print ("=============================================================",
+        "\n===                         REGLAS                        ===",
+        "\n=============================================================",
+        "\n-- • Debes seleccionar 6 ingredientes",
+        "\n-- •	No puedes repetir ingredientes",
+        "\n-- •	Tienes 3 vidas",
+        "\n-- •	Si eliges mal, la vacuna tenerdra efectos secundarios",
+        "\n-- •	Si eliges correctamente, salvas a la humanidad")
+    input("\n--          Presiona enter para continuar          ---")
+    
+#  Mostrar ingredientes para la vacuna
+def MostrarIngredientes():
+    print ("\n======================================================",
+        "\n===             FABRICACION DE VACUNAS            ===",
+        "\n======================================================",
+        "\n-- Ingredientes disponibles:",
+        "\n-- 1.",
+        "\n-- 2.",
+        "\n-- 3.",
+        "\n-- 4.",
+        "\n-- 5.",
+        "\n-- 6.",
+        "\n-- 7.",
+        "\n-- 8." \
+        "\n-- 9.",
+        "\n-- 10. Caldo de pollito.",
+        "\n-- Seleccione 6 ingredientes:")
