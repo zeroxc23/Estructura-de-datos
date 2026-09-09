1b56b16195d874bfcbc85a514a29f89769dac147
Jeshua Garcia <jeshuagarcia.7mo@gmail.com>
2026-09-09 17:06:49 -0500

    Segunda Fase

---
+++ Vacuna/Vacuna.py
@@ -0,0 +1,30 @@
+import time
+import os
+
+print ("============================================================",
+    "\n===                        HISTORIA                       ===",
+    "\n=============================================================",
+    "\nUn virus desconocido ha puesto en peligro a la humanidad.",
+    "\nTú, como científico, debes fabricar una vacuna utilizando 6",
+    "\ningredientes. Pero no es tan fácil... Debes elegir los ingredientes correctos",
+    "\npara que la vacuna funcione.",
+    "\n¡El destino del mundo está en tus manos!")
+
+input("\n--          Presiona enter para continuar          ---")
+time.sleep(1)
+os.system("cls")
+
+print ("\n=====================================================",
+    "\n===             FABRICACION DE VACUNAS            ===",
+    "\n======================================================",
+    "\n-- Ingredientes disponibles:",
+    "\n-- 1.",
+    "\n-- 2.",
+    "\n-- 3.",
+    "\n-- 4.",
+    "\n-- 5.",
+    "\n-- 6.",
+    "\n-- 7.",
+    "\n-- 8." \
+    "\n-- 9.",
+    "\n-- 10. Caldo de pollito.",
